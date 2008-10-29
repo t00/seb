ac0871f359b72553885ec61bee372b36ce564ebd
bschmuck <bschmuck@0c343a18-9ee7-46a8-87ea-1d1407b43e6f>
2008-10-29 13:55:25 +0000

    Udpdate Website. Index.html updated.
    git-svn-id: http://svn.code.sf.net/p/seb/svn/trunk@54 0c343a18-9ee7-46a8-87ea-1d1407b43e6f

---
+++ website/About_files/About.js
@@ -0,0 +1,11 @@
+// Created by iWeb 2.0.4 local-build-20081029
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.500000}),stroke_0:new IWStrokeParts([{rect:new IWRect(-1,1,2,93),url:'About_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'About_files/stroke_1.png'},{rect:new IWRect(1,-1,519,2),url:'About_files/stroke_2.png'},{rect:new IWRect(520,-1,2,2),url:'About_files/stroke_3.png'},{rect:new IWRect(520,1,2,93),url:'About_files/stroke_4.png'},{rect:new IWRect(520,94,2,3),url:'About_files/stroke_5.png'},{rect:new IWRect(1,94,519,3),url:'About_files/stroke_6.png'},{rect:new IWRect(-1,94,2,3),url:'About_files/stroke_7.png'}],new IWSize(521,95)),reflection_0:new IWReflection({opacity:0.50,offset:1.00})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('About_files/AboutMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ website/Documentation_Developper_files/Documentation_Developper.js
@@ -0,0 +1,11 @@
+// Created by iWeb 2.0.4 local-build-20081029
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-1,1,2,95),url:'Documentation_Developper_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'Documentation_Developper_files/stroke_1.png'},{rect:new IWRect(1,-1,531,2),url:'Documentation_Developper_files/stroke_2.png'},{rect:new IWRect(532,-1,2,2),url:'Documentation_Developper_files/stroke_3.png'},{rect:new IWRect(532,1,2,95),url:'Documentation_Developper_files/stroke_4.png'},{rect:new IWRect(532,96,2,3),url:'Documentation_Developper_files/stroke_5.png'},{rect:new IWRect(1,96,531,3),url:'Documentation_Developper_files/stroke_6.png'},{rect:new IWRect(-1,96,2,3),url:'Documentation_Developper_files/stroke_7.png'}],new IWSize(533,97)),reflection_0:new IWReflection({opacity:0.50,offset:1.00}),shadow_0:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.500000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Documentation_Developper_files/Documentation_DevelopperMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
