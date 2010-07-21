c9bd6b35eca314ee9f0c942854c48313abc96bf2
dirkba <dirkba@0c343a18-9ee7-46a8-87ea-1d1407b43e6f>
2010-07-21 15:49:54 +0000

    Licence text added
    git-svn-id: http://svn.code.sf.net/p/seb/svn/trunk@184 0c343a18-9ee7-46a8-87ea-1d1407b43e6f

--- win/Seb/Seb/Seb.h
+++ win/Seb/Seb/Seb.h
@@ -25,53 +25,86 @@
 
 #pragma once
 
-#include "Resource.h"
-#include "SystemVersionInfo.h"
+#include "resource.h"
+#include "../SystemVersionInfo.h"
 
-#define IDM_START 9001
+// Name and location of Seb configuration file
+#define SEB_INI "../Seb.ini"
+
+#define IDM_START   9001
 #define IDM_OFFSET 37265
 
 #define MAX_LOADSTRING 100
-#define SEB_DESK "SEB Desktop"
+#define SEB_DESK       "SEB Desktop"
+#define WINKEYOX_DESK  "WinKeyox Desktop"
 
 //System Messages
-#define	FILE_NOT_FOUND "File not found!"
-#define	PATH_NOT_FOUND "Path not found!"
-#define	ACCESS_DENIED "Access denied!"
-#define	UNDEFINED_ERROR "Undefined error in CheckWritePermission!"
-#define NO_WRITE_PERMISSION "No write permission!"
-#define	NO_INI_ERROR "No *.ini File found!"
-#define	NO_CLIENT_INFO_ERROR "No client info!"
-#define	INITIALIZE_ERROR "Initialization failed!"
-#define	REG_EDIT_ERROR "Error editing the registry!"
+#define	FILE_NOT_FOUND           "File not found!"
+#define	PATH_NOT_FOUND           "Path not found!"
+#define	ACCESS_DENIED            "Access denied!"
+#define	UNDEFINED_ERROR          "Undefined error in CheckWritePermission!"
+#define NO_WRITE_PERMISSION      "No write permission!"
+#define	NO_INI_ERROR             "No *.ini File found!"
+#define	NO_CLIENT_INFO_ERROR     "No client info!"
+#define	INITIALIZE_ERROR         "Initialization failed!"
+#define	REG_EDIT_ERROR           "Error editing the registry!"
+
 #define	NOT_ENOUGH_REGISTRY_RIGHTS_ERROR "Not enough rights to edit registry keys!"
-#define REGISTRY_WARNING "Registry Warning!"
-#define PROCESS_FAILED "Process call failed!"
+
+#define REGISTRY_WARNING         "Registry Warning!"
+#define PROCESS_FAILED           "Process call failed!"
+
 #define PROCESS_WINDOW_NOT_FOUND "No process window found!"
-#define LOAD_LIBRARY_ERROR "Could not load library!"
-#define	NO_LANGSTRING_FOUND "No language string found!"
-#define	NO_INSTANCE "Could not get my instance!"
-#define	NO_FILE_ERROR "Could not create or find file!"
-#define	NO_TASKBAR_HANDLE "No taskbar handle!"
-#define	FIREFOX_START_FAILED "Could not start firefox!"
-#define KEYLOGGER_FAILED "Could not start KeyLogger!"
-#define	SEB_TERMINATED "SEB terminated!"
-#define	NO_OS_SUPPORT "The OS is not supported!"
-#define	KILL_PROC_FAILED "Killing process %s failed: %d"
+
+#define LOAD_LIBRARY_ERROR       "Could not load library!"
+#define	NO_LANGSTRING_FOUND      "No language string found!"
+#define	NO_INSTANCE              "Could not get my instance!"
+#define	NO_FILE_ERROR            "Could not create or find file!"
+#define	NO_TASKBAR_HANDLE        "No taskbar handle!"
+#define	FIREFOX_START_FAILED     "Could not start firefox!"
+#define KEYLOGGER_FAILED         "Could not start KeyLogger!"
+
+#define KIOX_TERMINATED          "Kiox terminated!"
+#define SEB_TERMINATED           "SEB terminated!"
+
+#define	NO_OS_SUPPORT            "The OS is not supported!"
+#define	KILL_PROC_FAILED         "Killing process %s failed: %d"
+
+
+// HKEY_CURRENT_USER contains the Windows Registry keys
+//
+// DisableLockWorkstation ("Lock this computer")
+// DisableChangePassword  ("Change a password")
+// DisableTaskMgr         ("Start Task Manager")
+// NoLogoff               ("Log off")
+//
+// HKEY_LOCAL_MACHINE contains the Windows Registry key
+//
+// HideFastUserSwitching  ("Switch User")
+
 #define HKCU HKEY_CURRENT_USER
 #define HKLM HKEY_LOCAL_MACHINE
-#define SHOW_WIN_HIDE 0
-#define SHOW_WIN_NORMAL 1
+
+#define SHOW_WIN_HIDE      0
+#define SHOW_WIN_NORMAL    1
 #define SHOW_WIN_MINIMIZED 2
 #define GINA_PATH TEXT("MSGINA.DLL")
 
-LPCTSTR KEY_RegPolicySystem = "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System";
+
+
+LPCTSTR KEY_RegPolicySystem   = "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System";
 LPCTSTR KEY_RegPolicyExplorer = "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer";
-LPCTSTR KEY_RegPolicySEB = "Software\\Policies\\SEB";
+LPCTSTR KEY_RegPolicySEB      = "Software\\Policies\\SEB";
+
+LPCTSTR VAL_HideFastUserSwitching  = "HideFastUserSwitching";
 LPCTSTR VAL_DisableLockWorkstation = "DisableLockWorkstation";
-LPCTSTR VAL_DisableTaskMgr = "DisableTaskMgr";
-LPCTSTR VAL_DisableChangePassword = "DisableChangePassword";
-LPCTSTR VAL_NoClose = "NoClose";
-LPCTSTR VAL_NoLogoff = "NoLogoff";
-LPCTSTR VAL_PERMITTED_APPS = "PermittedApps";
+LPCTSTR VAL_DisableChangePassword  = "DisableChangePassword";
+LPCTSTR VAL_DisableTaskMgr         = "DisableTaskMgr";
+LPCTSTR VAL_NoLogoff               = "NoLogoff";
+LPCTSTR VAL_NoClose                = "NoClose";
+
+
+// Only for trunk version necessary (XULrunner)
+
+LPCTSTR VAL_PERMITTED_APPS    = "PermittedApps";
 LPCTSTR VAL_ShowSEBAppChooser = "ShowSEBAppChooser";
