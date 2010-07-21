30a86490781d6b67c86edb2aba03682f165729d8
dirkba <dirkba@0c343a18-9ee7-46a8-87ea-1d1407b43e6f>
2010-07-21 16:07:34 +0000

    git-svn-id: http://svn.code.sf.net/p/seb/svn/trunk@187 0c343a18-9ee7-46a8-87ea-1d1407b43e6f

--- win/Seb/MsgHook/AlterFlags.h
+++ win/Seb/MsgHook/AlterFlags.h
@@ -23,39 +23,45 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-typedef struct {
+typedef struct
+{
 	BYTE	DISABLE_CTRL_ESC : 1,
+			DISABLE_CTRL_P  : 1,
 			DISABLE_ALT_TAB : 1,
 			DISABLE_ALT_ESC : 1,
-			DISABLE_ALT_F4 : 1,
-			DISABLE_START_MENU : 1,
+			DISABLE_ALT_F4  : 1,
+			DISABLE_START_MENU  : 1,
 			DISABLE_RIGHT_MOUSE : 1,
-			DISABLE_LEFT_MOUSE : 1,
-			DISABLE_F1 : 1,
-			DISABLE_F2 : 1,
-			DISABLE_F3 : 1,
-			DISABLE_F4 : 1,
-			DISABLE_F5 : 1,
-			DISABLE_F6 : 1,
-			DISABLE_F7 : 1,
-			DISABLE_F8 : 1,
-			DISABLE_F9 : 1,
+			DISABLE_LEFT_MOUSE  : 1,
+			DISABLE_F1  : 1,
+			DISABLE_F2  : 1,
+			DISABLE_F3  : 1,
+			DISABLE_F4  : 1,
+			DISABLE_F5  : 1,
+			DISABLE_F6  : 1,
+			DISABLE_F7  : 1,
+			DISABLE_F8  : 1,
+			DISABLE_F9  : 1,
 			DISABLE_F10 : 1,
 			DISABLE_F11 : 1,
 			DISABLE_F12 : 1,
 			DISABLE_ESCAPE : 1;
 } ALTER_FLAGS;
 
-#define HKLM HKEY_LOCAL_MACHINE
-
-LPCTSTR KEY_RegPolicySystem = "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System";
+LPCTSTR KEY_RegPolicySystem   = "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System";
 LPCTSTR KEY_RegPolicyExplorer = "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer";
-LPCTSTR KEY_RegPolicySEB = "Software\\Policies\\SEB";
+LPCTSTR KEY_RegPolicySEB      = "Software\\Policies\\SEB";
+
+LPCTSTR VAL_HideFastUserSwitching  = "HideFastUserSwitching";
 LPCTSTR VAL_DisableLockWorkstation = "DisableLockWorkstation";
-LPCTSTR VAL_DisableTaskMgr = "DisableTaskMgr";
-LPCTSTR VAL_DisableChangePassword = "DisableChangePassword";
-LPCTSTR VAL_NoClose = "NoClose";
-LPCTSTR VAL_NoLogoff = "NoLogoff";
+LPCTSTR VAL_DisableChangePassword  = "DisableChangePassword";
+LPCTSTR VAL_DisableTaskMgr         = "DisableTaskMgr";
+LPCTSTR VAL_NoLogoff               = "NoLogoff";
+LPCTSTR VAL_NoClose                = "NoClose";
+
 LPCTSTR VAL_Button1 = "B1";
 LPCTSTR VAL_Button2 = "B2";
 LPCTSTR VAL_Button3 = "B3";
+
+#define VK_P              0x50
+
