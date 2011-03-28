95be3d573c6b9fa44f980feb7c8725dc1970a41b
danschlet <danschlet@0c343a18-9ee7-46a8-87ea-1d1407b43e6f>
2011-03-28 16:21:08 +0000

    "Importing SafeExamBrowser-src (current source code representing SEB MacOSX version 1.0.2)"
    git-svn-id: http://svn.code.sf.net/p/seb/svn/trunk@358 0c343a18-9ee7-46a8-87ea-1d1407b43e6f

---
+++ mac/SafeExamBrowser-src/AboutView.h
@@ -0,0 +1,19 @@
+//
+//  aboutView.h
+//  SafeExamBrowser
+//
+//  Created by Daniel R. Schneider on 30.10.10.
+//  Copyright (c) 2010 __MyCompanyName__. All rights reserved.
+//
+
+// View class for the about panel
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface AboutView : NSView {
+@private
+    
+}
+
+@end
---
+++ mac/SafeExamBrowser-src/CapView.h
@@ -0,0 +1,19 @@
+//
+//  CapView.h
+//  SEB
+//
+//  Created by Daniel R. Schneider on 29.07.10.
+//  Copyright (c) 2010 ETH Zurich LET. All rights reserved.
+//
+
+// View class for the windows covering all attached screens with a black background
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface CapView : NSView {
+@private
+    
+}
+
+@end
