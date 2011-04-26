ce43a0e56b5abfaee80afb9dee4146baf03df480
sschneider6 <sschneider6@0c343a18-9ee7-46a8-87ea-1d1407b43e6f>
2011-04-26 14:05:50 +0000

    * enable popup "more colors" * beta: error message
    git-svn-id: http://svn.code.sf.net/p/seb/svn/trunk@445 0c343a18-9ee7-46a8-87ea-1d1407b43e6f

--- win/Seb/xul_seb/defaults/preferences/prefs.js
+++ win/Seb/xul_seb/defaults/preferences/prefs.js
@@ -10,3 +10,8 @@ pref("seb.startup.homepage", "http://www.safeexambrowser.org");
 //browser
 pref("browser.download.manager.useWindow", false); 
 pref("browser.download.manager.retention", 0);
+
+pref("browser.link.open_newwindow", 2);
+pref("privacy.popups.policy", 1);
+pref("dom.disable_open_during_load", false);
+pref("browser.chromeURL", "chrome://xul_seb/content/seb.xul");
