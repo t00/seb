9b71e8eeb4a0941b1d35d6ba0dbfa8a3b30e68d0
dirkba <dirkba@0c343a18-9ee7-46a8-87ea-1d1407b43e6f>
2011-05-05 13:15:00 +0000

    git-svn-id: http://svn.code.sf.net/p/seb/svn/trunk@537 0c343a18-9ee7-46a8-87ea-1d1407b43e6f

--- win/SebWindowsPackage/RegistryEnableAllBlueScreenButtons.bat
+++ win/SebWindowsPackage/RegistryEnableAllBlueScreenButtons.bat
@@ -1,4 +1,15 @@
 
+REG DELETE "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System"   /v HideFastUserSwitching  /f
+REG DELETE "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System"   /v DisableLockWorkstation /f
+REG DELETE "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System"   /v DisableChangePassword  /f
+REG DELETE "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System"   /v DisableTaskMgr         /f
+REG DELETE "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoLogoff               /f
+REG DELETE "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoClose                /f
+REG DELETE "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v EnableShade            /f
+REG DELETE "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\Utilman.exe" /v Debugger /f
+
+pause
+
 REG ADD    "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System"   /v HideFastUserSwitching  /t REG_DWORD /d 0x00000000 /f
 REG ADD    "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System"   /v DisableLockWorkstation /t REG_DWORD /d 0x00000000 /f
 REG ADD    "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System"   /v DisableChangePassword  /t REG_DWORD /d 0x00000000 /f
@@ -6,7 +17,7 @@ REG ADD    "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System"   /v
 REG ADD    "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoLogoff               /t REG_DWORD /d 0x00000000 /f
 REG ADD    "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoClose                /t REG_DWORD /d 0x00000000 /f
 REG ADD    "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v EnableShade            /t REG_DWORD /d 0x00000001 /f
-REG DELETE "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\Utilman.exe"                            /f
+REG ADD    "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\Utilman.exe" /v Debugger /t REG_SZ /d "" /f
 
 pause
 
