75a4f41f299e59edcb966a6e4518ae04e6dac420
dirkba <dirkba@0c343a18-9ee7-46a8-87ea-1d1407b43e6f>
2011-05-09 15:17:45 +0000

    git-svn-id: http://svn.code.sf.net/p/seb/svn/trunk@586 0c343a18-9ee7-46a8-87ea-1d1407b43e6f

---
+++ win/SebWindowsPackage/Seb/xul_seb/defaults/preferences/prefs.js
@@ -0,0 +1,12 @@
+pref("toolkit.defaultChromeURI", "chrome://xul_seb/content/seb.xul");
+
+//seb useragent.extra
+pref("general.useragent.extra.firefox", "SEB");
+
+//seb
+pref("seb.configuration.file", "D:\\Seb.ini");
+pref("seb.startup.homepage", "http://www.safeexambrowser.org");
+
+//browser
+pref("browser.download.manager.useWindow", false); 
+pref("browser.download.manager.retention", 0);
