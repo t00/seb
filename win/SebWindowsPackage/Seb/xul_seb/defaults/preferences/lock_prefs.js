6416fc11ba038932ece42fe756b49a6b1c54852c
dirkba <dirkba@0c343a18-9ee7-46a8-87ea-1d1407b43e6f>
2011-05-10 15:14:01 +0000

    git-svn-id: http://svn.code.sf.net/p/seb/svn/trunk@590 0c343a18-9ee7-46a8-87ea-1d1407b43e6f

---
+++ win/SebWindowsPackage/Seb/xul_seb/defaults/preferences/lock_prefs.js
@@ -0,0 +1,8 @@
+//TODO
+//pref("general.config.filename", "mozilla.cfg");
+
+//workaround
+lock_pref("app.update.auto", false);
+lock_pref("app.update.enabled", false);
+lock_pref("browser.startup.homepage_override.mstone", "ignore");
+lock_pref("extensions.update.enabled", false);
---
+++ win/SebWindowsPackage/SebWindowsService/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace SebWindowsService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+			{ 
+				new SebWindowsService() 
+			};
+            ServiceBase.Run(ServicesToRun);
+/*
+            DebugOutputLine(true, "");
+            DebugOutputLine(true, "Enter Program::Main()");
+            DebugOutputLine(true, "Leave Program::Main()");
+            DebugOutputLine(true, "");
+*/
+        }
+    }
+}
