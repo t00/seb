860b50777923821e8135e6253b05715436cb8f44
dirkba <dirkba@0c343a18-9ee7-46a8-87ea-1d1407b43e6f>
2011-05-19 10:24:54 +0000

    The /Seb directory is now located in the /SebWindowsPackage directory
    git-svn-id: http://svn.code.sf.net/p/seb/svn/trunk@616 0c343a18-9ee7-46a8-87ea-1d1407b43e6f

---
+++ win/SebObsolete/Seb/resource.h
@@ -0,0 +1,31 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by Seb.rc
+//
+#define IDC_MYICON                      2
+#define IDD_SEB_DIALOG                  102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDC_SEB                         109
+#define IDR_MAINFRAME                   128
+#define IDI_SEB                         132
+#define IDI_ICON1                       138
+#define IDI_ICON2                       140
+#define IDI_ICON3                       142
+#define ID_FILE_STARTPROCESS            32771
+#define IDM_STARTPROCESS                32772
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        143
+#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
---
+++ win/SebObsolete/xul_seb/defaults/preferences/prefs.js
@@ -0,0 +1,20 @@
+pref("toolkit.defaultChromeURI", "chrome://xul_seb/content/seb.xul");
+
+//seb useragent.extra
+pref("general.useragent.extra.firefox", "SEB");
+
+//seb
+pref("seb.configuration.file", "D:\\Seb.ini");
+pref("seb.startup.homepage", "http://www.safeexambrowser.org");
+
+//browser
+pref("browser.download.manager.useWindow", false); 
+pref("browser.download.manager.retention", 0);
+
+pref("browser.link.open_newwindow", 2);
+pref("privacy.popups.policy", 1);
+pref("dom.disable_open_during_load", false);
+pref("browser.chromeURL", "chrome://xul_seb/content/seb.xul");
+
+pref("intl.locale.matchOS", "false");
+pref("general.useragent.locale", "en-US");
