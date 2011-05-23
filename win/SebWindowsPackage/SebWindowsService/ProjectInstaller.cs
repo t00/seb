bbf90431089101b0d64d495cac5d628dfa28a476
dirkba <dirkba@0c343a18-9ee7-46a8-87ea-1d1407b43e6f>
2011-05-23 14:22:41 +0000

    git-svn-id: http://svn.code.sf.net/p/seb/svn/trunk@646 0c343a18-9ee7-46a8-87ea-1d1407b43e6f

--- win/SebWindowsPackage/SebWindowsService/ProjectInstaller.cs
+++ win/SebWindowsPackage/SebWindowsService/ProjectInstaller.cs
@@ -19,6 +19,11 @@ namespace SebWindowsService
 
         private void SebServiceInstaller_Committed(object sender, InstallEventArgs e)
         {
+            // Unzip the XULRunner directories after installation
+            string XulRunnerDirectory = "xul.zip";
+            Console.WriteLine("XulRunnerDirectory = " + XulRunnerDirectory);
+            //System("copy xul.zip xul2.zip");
+
             // Autostart the SEB Windows Service when installation was successful
             string sebServiceName       = this.SebServiceInstaller.ServiceName;
             var    sebServiceController = new ServiceController(sebServiceName);
--- win/SebWindowsPackage/SebWindowsService/Properties/AssemblyInfo.cs
+++ win/SebWindowsPackage/SebWindowsService/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SebWindowsService")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Part of Safe Exam Browser")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("ETH Zurich")]
 [assembly: AssemblyProduct("SebWindowsService")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCopyright("Copyright © ETH Zurich 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.5.2.0")]
+[assembly: AssemblyFileVersion("1.5.2.0")]
