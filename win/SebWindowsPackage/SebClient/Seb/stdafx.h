f82c2656c0013770db8565b0d5f0f7d9a9fc9076
dirkba <dirkba@0c343a18-9ee7-46a8-87ea-1d1407b43e6f>
2011-06-15 07:42:36 +0000

    Renamed /Seb to /SebClient, renamed /Seb/Seb to /SebClient/SebStarter
    git-svn-id: http://svn.code.sf.net/p/seb/svn/trunk@681 0c343a18-9ee7-46a8-87ea-1d1407b43e6f

---
+++ win/SebWindowsPackage/SebClient/Seb/stdafx.h
@@ -0,0 +1,79 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is the include file for standard system include files,
+ * or project specific include files that are used frequently, but
+ * are changed infrequently.
+ *
+ * The Initial Developer of the Original Code is Justus-Liebig-Universitaet Giessen.
+ * Portions created by the Initial Developer are Copyright (C) 2005
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Stefan Schneider <stefan.schneider@hrz.uni-giessen.de>
+ *   Oliver Rahs <rahs@net.ethz.ch>
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+#pragma once
+
+
+// Suppresses disturbing compiler warnings regarding "strcpy" and "strcat"
+#define _CRT_SECURE_NO_WARNINGS
+
+
+// Modify the following defines if you have to target a platform prior to the ones specified below.
+// Refer to MSDN for the latest info on corresponding values for different platforms.
+
+#ifndef WINVER				// Allow use of features specific to Windows 95 and Windows NT 4 or later.
+#define WINVER 0x0501		// Change this to the appropriate value to target Windows 98 and Windows 2000 or later.
+#endif
+
+#ifndef _WIN32_WINDOWS		  // Allow use of features specific to Windows 98 or later.
+#define _WIN32_WINDOWS 0x0410 // Change this to the appropriate value to target Windows Me or later.
+#endif
+
+#ifndef _WIN32_WINNT			// Allow use of features specific to Windows NT 4 or later.
+#define _WIN32_WINNT 0x0501		// Change this to the appropriate value to target Windows 98 and Windows 2000 or later.
+#endif
+
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+
+// Windows Header Files:
+#include <windows.h>
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+// TODO: reference additional headers your program requires here
+#include<winsock2.h>
+#include<process.h>
+#include<signal.h>
+#include<stdio.h>
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+#include<string>
+#include<map>
+#include <tlhelp32.h>
+#include <algorithm>
+#include <vector>
+#include <winwlx.h>
+
+#include <Sddl.h>       // ConvertSidToStringSid
+#include <atlstr.h>     // CString
+
+using namespace std;
