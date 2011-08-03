bb980b93d53dd61bd1e0d76849160e0527cde603
dirkba <dirkba@0c343a18-9ee7-46a8-87ea-1d1407b43e6f>
2011-08-03 15:18:45 +0000

    DotNetZip library by Cheeso (http://dotnetzip.codeplex.com/) unpacks the XULRunner zip files at the end of the Seb Windows installation.
    git-svn-id: http://svn.code.sf.net/p/seb/svn/trunk@854 0c343a18-9ee7-46a8-87ea-1d1407b43e6f

--- win/SebWindowsPackage/SebWindowsService/ProjectInstaller.cs
+++ win/SebWindowsPackage/SebWindowsService/ProjectInstaller.cs
@@ -6,6 +6,7 @@ using System.Configuration.Install;
 using System.Linq;
 using System.ServiceProcess;
 using System.IO.Packaging;
+using Ionic.Zip;
 
 
 
@@ -21,17 +22,61 @@ namespace SebWindowsService
 
         private void SebServiceInstaller_Committed(object sender, InstallEventArgs e)
         {
-            // Unzip the XULRunner directories after installation
-            //string XulSebZipFile = "xul_seb.zip";
-            //Console.WriteLine("XulSebZipFile = " + XulSebZipFile);
+            // Unpack the XULRunner directories after installation
 
-            //ZipFile zipFile = zipFile.Read("xul_seb.zip");
+            string ProgramData  = System.Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+            string ProgramFiles = System.Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
 
-            //System.IO.Packaging;
-            //System.IO.Compression.DeflateStream deflateStream = new System.IO.Compression.DeflateStream();
-            //System("copy xul.zip xul2.zip");
+            string Manufacturer = "ETH Zuerich";
+            string Product      = "SEB Windows";
+            string Version      = "1.7";
+            string Component    = "SebWindowsClient";
+            string Build        = "Release";
 
-            // Autostart the SEB Windows Service when installation was successful
+            string SebConfigDir    = ProgramData  + "\\" + Manufacturer + "\\" + Product + " " + Version;
+            string SebInstallDir   = ProgramFiles + "\\" + Manufacturer + "\\" + Product + " " + Version + "\\" + Component + "\\" + Build;
+            string SebInstallDir86 = ProgramFiles + "\\" + Manufacturer + "\\" + Product + " " + Version + "\\" + Component + "\\" + Build;
+            string SebClientDir    = ProgramFiles + "\\" + Manufacturer + "\\" + Product + " " + Version + "\\" + Component;
+            string SebClientDir86  = ProgramFiles + "\\" + Manufacturer + "\\" + Product + " " + Version + "\\" + Component;
+
+            string XulSebZip         = "xul_seb.zip";
+            string XulRunnerZip      = "xulrunner.zip";
+            string XulRunnerNoSslZip = "xulrunner_no_ssl_warning.zip";
+
+            string XulSebZipFile         = SebClientDir + "\\" + XulSebZip;
+            string XulRunnerZipFile      = SebClientDir + "\\" + XulRunnerZip;
+            string XulRunnerNoSslZipFile = SebClientDir + "\\" + XulRunnerNoSslZip;
+
+
+            // Extract all files from the "xul_seb.zip" file
+            using (ZipFile zipFile = ZipFile.Read(XulSebZipFile))
+            {
+                foreach (ZipEntry zipEntry in zipFile)
+                {
+                    zipEntry.Extract(SebClientDir, ExtractExistingFileAction.OverwriteSilently);
+                }
+            }
+
+            // Extract all files from the "xulrunner.zip" file
+            using (ZipFile zipFile = ZipFile.Read(XulRunnerZipFile))
+            {
+                foreach (ZipEntry zipEntry in zipFile)
+                {
+                    zipEntry.Extract(SebClientDir, ExtractExistingFileAction.OverwriteSilently);
+                }
+            }
+
+            // Extract all files from the "xulrunner.zip" file
+            using (ZipFile zipFile = ZipFile.Read(XulRunnerNoSslZipFile))
+            {
+                foreach (ZipEntry zipEntry in zipFile)
+                {
+                    zipEntry.Extract(SebClientDir, ExtractExistingFileAction.OverwriteSilently);
+                }
+            }
+
+
+            // Autostart the SEB Windows Service after installation
             string sebServiceName       = this.SebServiceInstaller.ServiceName;
             var    sebServiceController = new ServiceController(sebServiceName);
                    sebServiceController.Start();
