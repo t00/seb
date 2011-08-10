3c498886c90a57d367c88b3b4f33a9de3e0140de
dirkba <dirkba@0c343a18-9ee7-46a8-87ea-1d1407b43e6f>
2011-08-10 13:39:27 +0000

    help utility to manually reinstall the SebWindowsService
    git-svn-id: http://svn.code.sf.net/p/seb/svn/trunk@890 0c343a18-9ee7-46a8-87ea-1d1407b43e6f

---
+++ win/SebWindowsPackage/SebWindowsInstall/SebWindowsInstallUtil.bat
@@ -0,0 +1,12 @@
+
+
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe "C:\Program Files (x86)\ETH Zuerich\SEB Windows 1.7\SebWindowsService\SebWindowsService.exe"
+
+
+
+
+echo.
+pause
+@echo on
+
+
