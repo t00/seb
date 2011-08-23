02f7349a1b955bf56178dcf02accb7a7ad8b2af7
dirkba <dirkba@0c343a18-9ee7-46a8-87ea-1d1407b43e6f>
2011-08-23 10:43:48 +0000

    git-svn-id: http://svn.code.sf.net/p/seb/svn/trunk@911 0c343a18-9ee7-46a8-87ea-1d1407b43e6f

--- win/SebWindowsPackage/SebWindowsInstall/SebWindowsCleanup.bat
+++ win/SebWindowsPackage/SebWindowsInstall/SebWindowsCleanup.bat
@@ -4,46 +4,8 @@
 
 echo.
 echo.
-echo Safe Exam Browser installation
-echo ------------------------------
-echo.
-echo For a full installation, you must execute the
-echo.
-echo SebWindowsInstall.bat
-echo.
-echo script when logged in as an administrator, not as a standard user!
-echo.
-echo Reason:
-echo.
-echo This script first runs the MSI installer with the
-echo installation file SebWindowsInstall.msi.
-echo.
-echo Afterwards, the script copies the configuration files
-echo MsgHook.ini and SebStarter.ini to the Program Data directory
-echo (overwriting the default MsgHook.ini and SebStarter.ini).
-echo.
-echo [And if desired, the script copies also the batch file
-echo SebStarter.bat to the Program Files directory,
-echo where SEB has been installed.]
-echo.
-echo If you execute this script as a standard user, the MSI installer
-echo will prompt you for an administrator password.
-echo Even after successful installation in administrator mode,
-echo the script will then fall back to standard user rights.
-echo.
-echo So the SebStarter.ini and MsgHook.ini files will _not_ be copied to
-echo the Program Data directory due to lack of administrator rights!
-echo In this case, you will have to _manually_ copy these .ini files
-echo as an administrator afterwards.
-echo.
-echo Solution:
-echo.
-echo To avoid this, please execute the SebWindowsInstall.bat
-echo script by right-clicking on its name in the Explorer,
-echo and then choosing "Run as administrator".
-echo You will then be prompted for the administrator password,
-echo and the whole script will be executed with administrator rights
-echo (running the MSI installer and copying the configured files)
+echo Safe Exam Browser cleanup
+echo -------------------------
 echo.
 
 
