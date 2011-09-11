21fe0a12af99faea90f892ed3475c67ae09191b2
danschlet <danschlet@0c343a18-9ee7-46a8-87ea-1d1407b43e6f>
2011-09-11 14:22:19 +0000

    imported current trunk/mac
    git-svn-id: http://svn.code.sf.net/p/seb/svn/trunk@935 0c343a18-9ee7-46a8-87ea-1d1407b43e6f

---
+++ mac/SafeExamBrowser-src/MyDocument.m
@@ -0,0 +1,104 @@
+//
+//  MyDocument.m
+//  SafeExamBrowser
+//
+//  Created by Daniel R. Schneider on 06.12.10.
+//  Copyright (c) 2010-2011 Daniel R. Schneider, ETH Zurich, 
+//  Educational Development and Technology (LET), 
+//  based on the original idea of Safe Exam Browser 
+//  by Stefan Schneider, University of Giessen
+//  Project concept: Thomas Piendl, Daniel R. Schneider, 
+//  Dirk Bauer, Karsten Burger, Marco Lehre, 
+//  Brigitte Schmucki, Oliver Rahs. French localization: Nicolas Dunand
+//
+//  ``The contents of this file are subject to the Mozilla Public License
+//  Version 1.1 (the "License"); you may not use this file except in
+//  compliance with the License. You may obtain a copy of the License at
+//  http://www.mozilla.org/MPL/
+//  
+//  Software distributed under the License is distributed on an "AS IS"
+//  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+//  License for the specific language governing rights and limitations
+//  under the License.
+//  
+//  The Original Code is Safe Exam Browser for Mac OS X.
+//  
+//  The Initial Developer of the Original Code is Daniel R. Schneider.
+//  Portions created by Daniel R. Schneider are Copyright 
+//  (C) 2010-2011 Daniel R. Schneider, ETH Zurich, Educational Development
+//  and Technology (LET), based on the original idea of Safe Exam Browser 
+//  by Stefan Schneider, University of Giessen. All Rights Reserved.
+//  
+//  Contributor(s): ______________________________________.
+//
+
+#import "MyDocument.h"
+
+@implementation MyDocument
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Add your subclass-specific initialization here.
+        // If an error occurs here, send a [self release] message and return nil.
+    }
+    return self;
+}
+
+
+- (NSString *)windowNibName
+{
+    // Override returning the nib file name of the document
+    // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this method and override -makeWindowControllers instead.
+    return @"MyDocument";
+}
+
+
+- (void)windowControllerDidLoadNib:(NSWindowController *)aController
+{
+    [super windowControllerDidLoadNib:aController];
+    // Add any code here that needs to be executed once the windowController has loaded the document's window.
+    //[webView setUIDelegate:self];
+    //[webView setGroupName:@"MyDocument"];
+}
+
+
+- (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError {
+    /*
+     Insert code here to write your document to data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning nil.
+    You can also choose to override -fileWrapperOfType:error:, -writeToURL:ofType:error:, or -writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
+    */
+    if (outError) {
+        *outError = [NSError errorWithDomain:NSOSStatusErrorDomain code:unimpErr userInfo:NULL];
+    }
+    return nil;
+}
+
+
+- (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError {
+    /*
+    Insert code here to read your document from the given data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning NO.
+    You can also choose to override -readFromFileWrapper:ofType:error: or -readFromURL:ofType:error: instead.
+    */
+    if (outError) {
+        *outError = [NSError errorWithDomain:NSOSStatusErrorDomain code:unimpErr userInfo:NULL];
+    }
+    return YES;
+}
+
+
+- (BOOL)isDocumentEdited
+{
+    // The browser windows are never edited
+    return NO;
+}
+
+
+- (id)webView
+{
+    return webView;
+}
+
+
+@end
