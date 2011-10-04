a5237b80a4d96338f981f31c3b5f4671cade11b0
dirkba <dirkba@0c343a18-9ee7-46a8-87ea-1d1407b43e6f>
2011-10-04 09:20:00 +0000

    git-svn-id: http://svn.code.sf.net/p/seb/svn/trunk@941 0c343a18-9ee7-46a8-87ea-1d1407b43e6f

---
+++ win/SebWindowsPackage/SebWindowsClient/chrome/content/js/seb.js
@@ -0,0 +1,188 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is the browser component of SEB.
+ *
+ * The Initial Developer of the Original Code is Stefan Schneider <stefan.schneider@uni-hamburg.de>.
+ * Portions created by the Initial Developer are Copyright (C) 2005
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Stefan Schneider <stefan.schneider@uni-hamburg.de>
+ *   Oliver Rahs <rahs@net.ethz.ch>
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+var SebSystem = {
+	_chrome :false,
+	_prefs :null,
+	_profile :null,
+	_dirUtils :null,
+	_chromeDir : null,
+	_appDir : null,
+	_sebIni : null,
+	_browser :null,
+	_locale  :null,
+	_const: null,
+
+	startup : function() {
+		try {
+			this._chrome = (top.window.toString().indexOf('ChromeWindow') > -1);
+			this._prefs = new Prefs();
+			this._profile = new Profile();
+			this._dirutils = new DirUtils();
+			this._browser = getBrowser();
+			this._chromeDir = new Dir(this._dirutils.getChromeDir());
+			this._appDir = this._chromeDir.parent.parent;
+			this._sebIni = this._appDir.clone();
+			this._sebIni.append(this._prefs.getChar('seb.configuration.filename'));
+			this._locale = document.getElementById("locale");
+			this._const = document.getElementById("const");
+			
+			/** **** EventListener ****** */
+			
+			if (this._chrome) {
+				var url = this.getUrl();
+				if (url == null || url == "") {
+					alert("A problem has occured:\nNo URL defined for SEB.");
+				}
+				initBrowser(url);
+			}
+		} catch (e) {
+			alert(e);
+		}
+	},
+
+	shutdown : function() {
+	},
+
+	showLoading : function() {
+		if (this._browser.contentWindow.location.href == "about:blank") {
+			document.getElementById("deckContents").selectedIndex = 0;
+		}
+	},
+	
+	showContent : function() {
+		document.getElementById("deckContents").selectedIndex = 1;
+		window.title = this._browser.webNavigation.document.title;
+		this._browser.focus();
+		var win = SebSystem._browser.contentWindow.wrappedJSObject;
+		//var doc = SebSystem._browser.contentDocument.wrappedJSObject;
+		win.open = SebSystem.openWin; // capture window.open event and use seb handler
+	},
+	
+	/* this is a first version of implementing popup windows
+	 * It works p.e. for simple popup windows like "Sonderzeichen Tabelle" or "more colors" in ILIAS tinyMCE
+	 * Complex websites with multiple open requests or ajax calls are not supported
+	*/
+	openWin : function(url, name, features) {
+		try {
+			features = "chrome,centerscreen,"+ features; // add arguments for new chrome winow 
+			//alert(url + "|" + name + "|" + features);
+			var openerWin = SebSystem._browser.contentWindow; 
+			var win = window.open(url,name,features); //  open the new window and pass the new features 
+			// tricky: 
+			// this tells the new window, "you are opened by browsers contentWindow and not by me"  ;-) 
+			// important for communication between the two windows
+			win.opener = openerWin; 
+			// bind window.open events of the popup window to the same handler
+			win.open = SebSystem.openWin; 
+			//win.addEventListener("close",SebSystem.closeWin,false);
+			win.focus();
+			// !IMPORTANT:
+			// WYSIWYG editors like tinMCE check the return value and send a popup blocking warning 
+			return true; 
+		}
+		catch (e) {
+			//alert(e);
+		}
+	},
+	
+	closeWin : function(e) {
+		//alert("closeWin" + e);
+	},
+	/** ***** Internal Callback functions ******* */
+	
+	getUrl : function() {
+		var url = "";
+		if (window.arguments[0] != null) {
+			var cmdLine = window.arguments[0].QueryInterface(Components.interfaces.nsICommandLine);
+			url = cmdLine.handleFlagWithParam("url", false);
+		}
+		if (url == null || url == "") {
+			url = this.getUrlExamFromWindowsRegistry();
+		}
+		if (url == null || url == "") {
+			url = this.getUrlExamFromFileSebIni();
+		}
+		if (url == null || url == "") {
+			url = this.getUrlExamFromPreferences();
+		}
+		return url;
+	},
+
+	getUrlExamFromWindowsRegistry : function() {
+		try {
+			var urlExam = "";
+			var reg = Components.classes["@mozilla.org/windows-registry-key;1"].createInstance(Components.interfaces.nsIWindowsRegKey);
+			reg.open(reg.ROOT_KEY_LOCAL_MACHINE, "SOFTWARE\\Policies", reg.ACCESS_READ);
+			if (reg.hasChild("SEB")) {
+				var subKey = reg.openChild("SEB", reg.ACCESS_READ);
+				if (subKey.hasValue("UrlExam")) {
+					urlExam = subKey.readStringValue("UrlExam");
+				}
+			}
+			reg.close();
+			return urlExam;
+		} catch (ex) {
+			return "";
+		}
+	},
+
+	getUrlExamFromFileSebIni : function() {
+		try {
+			var urlExam = "";
+			// first try to load hard coded path to *.ini file in prefs.js
+			var sebConfig = new File(this._prefs.getChar('seb.configuration.file'));
+			// if not exist get file with 'seb.configuration.filename' in app directory
+			if (!sebConfig.exists()) {
+				sebConfig = new File(this._sebIni.path); 
+			}
+			var lines = sebConfig.readAllLines();
+			for ( var i = 0; i < lines.length; i++) {
+				var reg = /^\s?URL_EXAM\s?=\s?(.+)?\s?$/;
+				var ret = reg.exec(lines[i]);
+				if (ret) {
+					urlExam = ret[1];
+				}
+			}
+			return urlExam;
+		} catch (ex) {
+			return "";
+		}
+	},
+
+	getUrlExamFromPreferences : function() {
+		return this._prefs.getChar('seb.startup.homepage');
+	}
+}
+
+/** ****** Top EventListener ***** */
+
+window.addEventListener("load", function(e) {
+	SebSystem.startup();
+}, false);
+
+window.addEventListener("unload", function(e) {
+	SebSystem.shutdown();
+}, false);
