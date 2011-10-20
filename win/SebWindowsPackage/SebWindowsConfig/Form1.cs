a32cea0abc2d52aaaf3511dcd7d251635875a3a1
dirkba <dirkba@0c343a18-9ee7-46a8-87ea-1d1407b43e6f>
2011-10-20 09:26:20 +0000

    git-svn-id: http://svn.code.sf.net/p/seb/svn/trunk@993 0c343a18-9ee7-46a8-87ea-1d1407b43e6f

---
+++ win/SebWindowsPackage/SebWindowsConfig/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SebWindowsConfig
+{
+    public partial class Form1 : Form
+    {
+        String sebStarterIniPath = "";
+        String sebMsgHookIniPath = "";
+        StreamReader streamReaderSebStarterIni;
+        StreamWriter streamWriterSebStarterIni;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox2.Checked = !checkBox1.Checked;
+        }
+
+        private void labelBrowseSebStarterFolder_Click(object sender, EventArgs e)
+        {
+            DialogResult browseFolderName = folderBrowserDialogBrowseSebStarterIni.ShowDialog();
+        }
+
+        private void labelOpenSebStarterIniFile_Click(object sender, EventArgs e)
+        {
+            DialogResult openFileDialogResult = openFileDialogSebStarterIni.ShowDialog();
+            sebStarterIniPath                 = openFileDialogSebStarterIni.FileName;
+            labelSebStarterIniPath.Text       = sebStarterIniPath;
+
+            try 
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (StreamReader sr = new StreamReader(sebStarterIniPath)) 
+                {
+                    String line;
+                    // Read and display lines from the file until the end of 
+                    // the file is reached.
+                    while ((line = sr.ReadLine()) != null) 
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception streamReadException) 
+            {
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(streamReadException.Message);
+            }
+
+        }
+
+        private void labelSaveSebStarterIniFile_Click(object sender, EventArgs e)
+        {
+            DialogResult saveFileName = saveFileDialogSebStarterIni.ShowDialog();
+        }
+    }
+}
