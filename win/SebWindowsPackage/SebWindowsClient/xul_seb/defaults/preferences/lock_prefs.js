c33a306b37b8dccb1874daacc6aff03cab179696
dirkba <dirkba@0c343a18-9ee7-46a8-87ea-1d1407b43e6f>
2012-03-13 13:54:52 +0000

    git-svn-id: http://svn.code.sf.net/p/seb/svn/trunk@1353 0c343a18-9ee7-46a8-87ea-1d1407b43e6f

--- win/SebWindowsPackage/SebWindowsClient/xul_seb/defaults/preferences/lock_prefs.js
+++
@@ -1,8 +0,0 @@
-//TODO
-//pref("general.config.filename", "mozilla.cfg");
-
-//workaround
-lock_pref("app.update.auto", false);
-lock_pref("app.update.enabled", false);
-lock_pref("browser.startup.homepage_override.mstone", "ignore");
-lock_pref("extensions.update.enabled", false);
