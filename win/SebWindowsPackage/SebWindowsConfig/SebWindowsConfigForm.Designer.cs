497800bd433419f5ba5479447e77c8be4720e214
dirkba <dirkba@0c343a18-9ee7-46a8-87ea-1d1407b43e6f>
2012-04-24 10:29:34 +0000

    git-svn-id: http://svn.code.sf.net/p/seb/svn/trunk@1385 0c343a18-9ee7-46a8-87ea-1d1407b43e6f

--- win/SebWindowsPackage/SebWindowsConfig/SebWindowsConfigForm.Designer.cs
+++ win/SebWindowsPackage/SebWindowsConfig/SebWindowsConfigForm.Designer.cs
@@ -640,9 +640,9 @@
             // 
             // buttonFinish
             // 
-            this.buttonFinish.Location = new System.Drawing.Point(16, 579);
+            this.buttonFinish.Location = new System.Drawing.Point(20, 580);
             this.buttonFinish.Name = "buttonFinish";
-            this.buttonFinish.Size = new System.Drawing.Size(189, 64);
+            this.buttonFinish.Size = new System.Drawing.Size(180, 60);
             this.buttonFinish.TabIndex = 52;
             this.buttonFinish.Text = "Configuration finished,\r\nclose window";
             this.buttonFinish.UseVisualStyleBackColor = true;
