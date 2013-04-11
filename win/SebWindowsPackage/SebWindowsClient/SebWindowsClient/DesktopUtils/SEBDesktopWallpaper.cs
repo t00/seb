2d237723f3a41b3b8a0deeed222c2fe42ab6ba78
dirkba <dirkba@0c343a18-9ee7-46a8-87ea-1d1407b43e6f>
2013-04-11 13:38:19 +0000

    Added new SebWindowsClient project after .NET porting
    git-svn-id: http://svn.code.sf.net/p/seb/svn/trunk@1674 0c343a18-9ee7-46a8-87ea-1d1407b43e6f

---
+++ win/SebWindowsPackage/SebWindowsClient/SebWindowsClient/DesktopUtils/SEBDesktopWallpaper.cs
@@ -0,0 +1,63 @@
+﻿// -------------------------------------------------------------
+//     Viktor tomas
+//     BFH-TI, http://www.ti.bfh.ch
+//     Biel, 2012
+// -------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace SebWindowsClient.DesktopUtils
+{
+    public class SEBDesktopWallpaper
+    {
+        const int SPI_SETDESKWALLPAPER = 20;
+        const int SPIF_UPDATEINIFILE = 0x01;
+        const int SPIF_SENDWININICHANGE = 0x02;
+
+        public SEBDesktopWallpaper()
+        {
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(
+            int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        public enum Style : int
+        {
+            Tiled, Centered, Stretched
+        }
+
+        public void SetWallpaper(string path, Style style)
+        {
+            if (System.IO.File.Exists(path))
+            {
+                RegistryKey key = Registry.CurrentUser.OpenSubKey("Control Panel\\Desktop", true);
+
+                switch (style)
+                {
+                    case Style.Stretched:
+                        key.SetValue(@"WallpaperStyle", "2");
+                        key.SetValue(@"TileWallpaper", "0");
+                        break;
+                    case Style.Centered:
+                        key.SetValue(@"WallpaperStyle", "1");
+                        key.SetValue(@"TileWallpaper", "0");
+                        break;
+                    case Style.Tiled:
+                        key.SetValue(@"WallpaperStyle", "1");
+                        key.SetValue(@"TileWallpaper", "1");
+                        break;
+                }
+                SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+            }
+            else
+            {
+                throw new System.IO.FileNotFoundException();
+            }
+        }
+    }
+}
---
+++ win/SebWindowsPackage/SebWindowsClient/SebWindowsClient/SebWindowsClientForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿using SebWindowsClient.BlockShortcutsUtils;
+namespace SebWindowsClient
+{
+    partial class SebWindowsClientForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            // Re-enable normal key events.
+            KeyCapture.FilterKeys = false;
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.lbl_User = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.Location = new System.Drawing.Point(406, 401);
+            this.btn_Exit.MaximumSize = new System.Drawing.Size(102, 23);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(102, 23);
+            this.btn_Exit.TabIndex = 0;
+            this.btn_Exit.Text = "Exit";
+            this.btn_Exit.UseVisualStyleBackColor = true;
+            this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
+            // 
+            // lbl_User
+            // 
+            this.lbl_User.AutoSize = true;
+            this.lbl_User.Dock = System.Windows.Forms.DockStyle.Right;
+            this.lbl_User.Location = new System.Drawing.Point(493, 0);
+            this.lbl_User.Name = "lbl_User";
+            this.lbl_User.Size = new System.Drawing.Size(45, 13);
+            this.lbl_User.TabIndex = 1;
+            this.lbl_User.Text = "lbl_User";
+            // 
+            // SebWindowsClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 436);
+            this.Controls.Add(this.lbl_User);
+            this.Controls.Add(this.btn_Exit);
+            this.Name = "SebWindowsClientForm";
+            this.Text = "Safe Exam Browser";
+            this.Load += new System.EventHandler(this.SebWindowsClientForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.Label lbl_User;
+    }
+}
+
