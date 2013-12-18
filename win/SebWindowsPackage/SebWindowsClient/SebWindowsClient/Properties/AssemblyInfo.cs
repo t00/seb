2d79b54479f844662c14d6b3efddea15fa645782
dirkba <dirkba@0c343a18-9ee7-46a8-87ea-1d1407b43e6f>
2013-12-18 16:44:52 +0000

    Changed assembly properties from SebWindowsClient to Safe Exam Browser and added SEB icon to properties of SafeExamBrowser.exe
    git-svn-id: http://svn.code.sf.net/p/seb/svn/trunk@2503 0c343a18-9ee7-46a8-87ea-1d1407b43e6f

--- win/SebWindowsPackage/SebWindowsClient/SebWindowsClient/Properties/AssemblyInfo.cs
+++ win/SebWindowsPackage/SebWindowsClient/SebWindowsClient/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SebWindowsClient")]
+[assembly: AssemblyTitle("Safe Exam Browser")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SebWindowsClient")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyProduct("Safe Exam Browser")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
