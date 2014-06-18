1f72332a142b462879fda4328d1817a5ef19d1ba
danschlet <danschlet@0c343a18-9ee7-46a8-87ea-1d1407b43e6f>
2014-06-18 14:01:35 +0000

    Added MPL license headers.
    git-svn-id: http://svn.code.sf.net/p/seb/svn/trunk@2884 0c343a18-9ee7-46a8-87ea-1d1407b43e6f

--- win/SebWindowsPackage/SebWindowsClient/SebWindowsClient/ThreadedDialog.cs
+++ win/SebWindowsPackage/SebWindowsClient/SebWindowsClient/ThreadedDialog.cs
@@ -7,6 +7,40 @@ using System.Windows.Forms;
 using SebWindowsClient.ConfigurationUtils;
 using SebWindowsClient.DesktopUtils;
 
+//
+//  ThreadedDialog.cs
+//  SafeExamBrowser
+//
+//  Copyright (c) 2010-2014 Daniel R. Schneider, Pascal Wyss,
+//  ETH Zurich, Educational Development and Technology (LET),
+//  based on the original idea of Safe Exam Browser
+//  by Stefan Schneider, University of Giessen
+//  Project concept: Thomas Piendl, Daniel R. Schneider,
+//  Dirk Bauer, Kai Reuter, Tobias Halbherr, Karsten Burger, Marco Lehre,
+//  Brigitte Schmucki, Oliver Rahs. French localization: Nicolas Dunand
+//
+//  ``The contents of this file are subject to the Mozilla Public License
+//  Version 1.1 (the "License"); you may not use this file except in
+//  compliance with the License. You may obtain a copy of the License at
+//  http://www.mozilla.org/MPL/
+//
+//  Software distributed under the License is distributed on an "AS IS"
+//  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+//  License for the specific language governing rights and limitations
+//  under the License.
+//
+//  The Original Code is Safe Exam Browser for Windows.
+//
+//  The Initial Developers of the Original Code are Daniel R. Schneider, Pascal Wyss.
+//  Portions created by Daniel R. Schneider, Pascal Wyss
+//  are Copyright (c) 2010-2014 Daniel R. Schneider, Pascal Wyss, 
+//  ETH Zurich, Educational Development and Technology (LET), 
+//  based on the original idea of Safe Exam Browser
+//  by Stefan Schneider, University of Giessen. All Rights Reserved.
+//
+//  Contributor(s): ______________________________________.
+//
+
 namespace SebWindowsClient
 {
     public class Worker
